0b595820f52552777d3842a345b3eafe4a382551
alphabet5 <alphabet5@users.noreply.github.com>
2022-01-06 11:14:34 -0600

    Minimal changes. Better logging, paramterized input name.

--- archery-sight-tape.py
+++ archery-sight-tape.py
@@ -1,13 +1,17 @@
 from jinja2 import Template
 from yamlarg import parse
+from rich.traceback import install
+install(show_locals=True)
 
 class line:
-    def __init__(self, value, y, color):
+    def __init__(self, value, x, y, color, offset, line_width):
         self.value = value
         self.y = y
         self.id = value
-        self.texty = y + 7
+        self.texty = y + offset
         self.color = color
+        self.x = x
+        self.line_width = line_width
 
 
 if __name__ == '__main__':
@@ -16,7 +20,7 @@ if __name__ == '__main__':
     args = parse('arguments.yaml')
 
     sight_settings = dict()
-    with open('sight-settings.csv', 'r') as f:
+    with open(args['input'], 'r') as f:
         for row in f.readlines():
             distance, sight = row.split(',')
             sight_settings[int(distance)] = float(sight)
@@ -28,20 +32,32 @@ if __name__ == '__main__':
     small = list()
     medium = list()
 
+    ppi = args['ppi']
+
+    large_width_pct, medium_width_pct, small_width_pct = args['line_width'].split(',')
+    large_width = (100 - int(large_width_pct))/100 * width
+    medium_width = (100 - int(medium_width_pct))/100 * width
+    small_width = (100 - int(small_width_pct))/100 * width
+
+    large_color, medium_color, small_color = args['color'].split(',')
+
+    large_weight, medium_weight, small_weight = args['line_weight'].split(',')
+
     start_offset = args['start'] * args['ppi']
 
     for k, v in sight_settings.items():
-        y = (v * args['ppi']) + start_offset
+        y = (v * ppi) + start_offset
+
         if y < height:
             if k % 10 == 0:
-                numbers.append(line(k, y, args['large_color']))
+                numbers.append(line(k, large_width, y, large_color, args['font_y_offset'], large_weight))
             elif k % 5 == 0:
-                medium.append(line(k, y, args['medium_color']))
+                medium.append(line(k, medium_width, y, medium_color, args['font_y_offset'], medium_weight))
             else:
-                small.append(line(k, y, args['small_color']))
+                small.append(line(k, small_width, y, small_color, args['font_y_offset'], small_weight))
 
     print(sight_settings)
 
-    output = template.render(height=height, width=width, numbers=numbers, small=small, medium=medium)
+    output = template.render(height=height, width=width, numbers=numbers, small=small, medium=medium, font_size=args['font_size'])
     with open(args['output'], 'w') as f:
         f.write(output)
